eb072e917fcbd7846ba298731563d3810ca8b659
Darley Rodrigues <darleyramos96@gmail.com>
2025-06-13 11:28:34 -0300

    Implementação de buscar por Id

--- src/main/java/com/darley/cadastroalunos/controller/AlunoController.java
+++ src/main/java/com/darley/cadastroalunos/controller/AlunoController.java
@@ -2,10 +2,7 @@ package com.darley.cadastroalunos.controller;
 
 import com.darley.cadastroalunos.model.Aluno;
 import com.darley.cadastroalunos.repository.AlunoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -22,5 +19,10 @@ public class AlunoController {
     @PostMapping
     public Aluno salvar(@RequestBody Aluno aluno){
         return alunoRepository.save(aluno);
+    }
+
+    @GetMapping("{id}")
+    public Aluno buscarPorId(@PathVariable ("id") UUID id){
+        return alunoRepository.findById(id).orElse(null);
     }
 }

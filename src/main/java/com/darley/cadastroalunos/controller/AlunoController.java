e5586b7a34173d1e89b2cf2e872a20ec6bf26ad8
Darley Rodrigues <darleyramos96@gmail.com>
2025-06-13 20:01:31 -0300

    CRUD completo funcional finalizado

--- src/main/java/com/darley/cadastroalunos/controller/AlunoController.java
+++ src/main/java/com/darley/cadastroalunos/controller/AlunoController.java
@@ -4,6 +4,7 @@ import com.darley.cadastroalunos.model.Aluno;
 import com.darley.cadastroalunos.repository.AlunoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,5 +25,22 @@ public class AlunoController {
     @GetMapping("{id}")
     public Aluno buscarPorId(@PathVariable ("id") UUID id){
         return alunoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable ("id") UUID id){
+        alunoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") UUID id,
+                           @RequestBody Aluno aluno){
+        aluno.setId(id);
+        alunoRepository.save(aluno);
+    }
+
+    @GetMapping
+    public List<Aluno> listarTodos(){
+        return alunoRepository.findAll();
     }
 }
